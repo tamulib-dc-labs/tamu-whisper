cd728b69143acc418be9e44370ff2c45ae0f68bf
Mark Baggett <markpbaggett@gmail.com>
2025-07-03 09:36:33 -0500

    Create VTTs for Media Files with no audio.

--- README.md
+++ README.md
@@ -2,3 +2,44 @@
 
 Tool for initializing subtitle, caption, and transcript files and embedding metadata about the process according to
 [FADGI Metadata Guidelines](https://www.digitizationguidelines.gov/guidelines/FADGI_WebVTT_embed_guidelines_v0.1_2024-04-18.pdf).
+
+## Requires
+
+* Python ~3.9 - Python ~3.12
+* ffmpeg
+
+## Running
+
+`tamuwhisper` is designed to run as a cli program versus a directory of media files like:
+
+```shell
+tamuwhisper transcribe -d path/to/files -m turbo -c "Surname, Given name " -o path/to/where/to/write/vtts
+```
+
+## Arguments
+
+```shell
+Usage: tamuwhisper transcribe [OPTIONS]
+
+  Create VTTs with FADGI Embedded Metadata and Whisper JSON
+
+Options:
+  -d, --directory TEXT            The path to your media files
+  -o, --output TEXT               The output directory to write your generated
+                                  vtts and json to
+  -m, --model [tiny|base|small|medium|large|turbo]
+                                  Which Whisper model to use
+  -l, --language TEXT             Specify language of media file
+  -f, --fp16                      Use FP16 instead of FP32
+  -i, --ignore-existing           Skip A/V files that already have a
+                                  transcription
+  -c, --creator TEXT              Name of person using the tool. Used for
+                                  embedded metadata.
+  --check_silence                 Run a check if files are silent first.
+  -t, --temperature FLOAT         Controls randomness in text decoding. Lower
+                                  values (e.g., 0.0) make output more
+                                  deterministic and accurate; higher values
+                                  (e.g., 1.0) increase diversity and may
+                                  improve results on noisy or ambiguous audio.
+  --help                          Show this message and exit.
+```
--- src/tamuwhisper.py
+++ src/tamuwhisper.py
@@ -15,24 +15,25 @@ def cli() -> None:
 
 
 @cli.command(
-    "transcribe", help="Create VTTs with FADGI Embedded Metadata and Whisper JSON"
+    "transcribe", 
+    help="Create VTTs with FADGI Embedded Metadata and Whisper JSON"
 )
 @click.option(
     "--directory",
     "-d",
-    help="The directory to read files from",
+    help="The path to your media files",
 )
 @click.option(
     "--output",
     "-o",
     default="output",
-    help="The output directory to write vtts and json to",
+    help="The output directory to write your generated vtts and json to",
 )
 @click.option(
     "--model",
     "-m",
     default="base",
-    help="The Whisper model to use",
+    help="Which Whisper model to use",
     type=click.Choice(
         ['tiny', 'base', 'small', 'medium', 'large', 'turbo']
     )
@@ -44,19 +45,31 @@ def cli() -> None:
     help="Specify language of media file",
 )
 @click.option(
-    '-f', '--fp16', is_flag=True, help='Use FP16 instead of FP32'
+    '-f',
+    '--fp16',
+    is_flag=True,
+    help='Use FP16 instead of FP32'
 )
 @click.option(
-    '-i', '--ignore-existing', is_flag=True,
-              help='Ignore A/V files that already have a transcription'
+    '-i', 
+    '--ignore-existing', 
+    is_flag=True,
+    help='Skip A/V files that already have a transcription'
 )
 @click.option(
     "-c",
     "--creator",
-    help="Name of person using the tool",
+    help="Name of person using the tool. Used for embedded metadata.",
     default="Unknown"
 )
-@click.option("--check_silence", is_flag=True, help="Check if files are silent first.")
+@click.option("--check_silence", is_flag=True, help="Run a check if files are silent first.")
+@click.option(
+    "-t", 
+    '--temperature', 
+    type=float, 
+    help='Controls randomness in text decoding. Lower values (e.g., 0.0) make output more deterministic and accurate; higher values (e.g., 1.0) increase diversity and may improve results on noisy or ambiguous audio.', 
+    default=0
+)
 def run(
         directory: str,
         output: str,
@@ -65,8 +78,11 @@ def run(
         fp16: bool,
         ignore_existing: bool,
         creator: str,
-        check_silence: bool
+        temperature: float,
+        check_silence: bool,
 ) -> None:
-    x = Transcriber(directory, output, model, language, fp16, ignore_existing, creator=creator, check_for_silence=check_silence)
+    x = Transcriber(
+        directory, output, model, language, fp16, ignore_existing, creator=creator, check_for_silence=check_silence, temperature=temperature
+        )
     x.batch_transcribe()
 
